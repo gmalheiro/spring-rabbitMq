164aae8cb9b375e3e8009462f22011664a3cfe48
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-12-21 21:57:43 -0300

    feat: add backend-worker project

---
+++ demo-backend-worker/src/main/java/com/gmalheiro/java/demo_backend_worker/DemoBackendWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.gmalheiro.java.demo_backend_worker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBackendWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBackendWorkerApplication.class, args);
+	}
+
+}
