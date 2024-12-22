47d8766d544c50c7489705f779b344b66d5fc0af
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-12-21 21:58:27 -0300

    feat: add backend-api project

---
+++ demo-backend-api/src/main/java/com/gmalheiro/java/demo_backend_api/DemoBackendApiApplication.java
@@ -0,0 +1,13 @@
+package com.gmalheiro.java.demo_backend_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBackendApiApplication.class, args);
+	}
+
+}
